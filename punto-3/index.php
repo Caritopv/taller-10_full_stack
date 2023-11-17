40ba08021acd0ff43b442fd1a90128aa11728e48
Caritopv <caritocpv@gmail.com>
2023-11-16 21:53:24 -0500

    punto-3

---
+++ punto-3/index.php
@@ -0,0 +1,14 @@
+<?php
+// INPUTS
+$n1 = 0;
+$n2 = 0;
+$result = 0;
+// PROCESS
+echo " digite el primer  numero  \n";
+$n1= readline();
+ echo " digite el segundo  numero  \n";
+$n2= readline();
+echo " La suma de los numeros \n";
+$result = $n1+$n2;
+echo $result;
+?>
